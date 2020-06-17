b86e82ed30fb471dee66492a24058b849df3bfd4
Jesse Schimmel <jhowardschimmel@gmail.com>
2020-06-16 23:29:13 -0400

    Add navbar component

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,26 +1,85 @@
-import React, { useState } from "react";
+
+import React from 'react';
+import { Fragment } from 'react';
 import {
+	Collapse,
 	Navbar,
+	NavbarToggler,
 	NavbarBrand,
 	Nav,
 	NavItem,
-	NavLink
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
 } from 'reactstrap';
 
-const Navigation = (props) => {
-	return (
-		<div>
+export default class Navigation extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+
+	render() {
+		return <Fragment>
 			<Navbar color="light" light expand="md">
 				<NavbarBrand href="/">Book Club</NavbarBrand>
-				<Nav>
-					<NavItem>
-                      <NavLink href="#">Currently Reading</NavLink>
-					</NavItem>
-				</Nav>
+				<NavbarToggler onClick={this.toggle} />
+				<Collapse isOpen={this.state.isOpen} navbar>
+					<Nav className="ml-auto" navbar>
+						<NavItem>
+							<NavLink href="/calendar">Calendar</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink href="/library">Library</NavLink>
+						</NavItem>
+						<UncontrolledDropdown nav inNavbar>
+							<DropdownToggle nav caret>
+								Members
+                </DropdownToggle>
+							<DropdownMenu right>
+								<DropdownItem>
+									Roster
+                  </DropdownItem>
+								<DropdownItem divider />
+								<DropdownItem>
+									Invite
+                  </DropdownItem>
+							</DropdownMenu>
+						</UncontrolledDropdown>
+
+						<UncontrolledDropdown nav inNavbar>
+							<DropdownToggle nav caret>
+								Books
+                </DropdownToggle>
+							<DropdownMenu right>
+								<DropdownItem>
+									Reading List
+                  </DropdownItem>
+								<DropdownItem divider />
+								<DropdownItem >
+									Nominate
+                  </DropdownItem>
+								<DropdownItem divider />
+								<DropdownItem>
+									Search
+                  </DropdownItem>
+							</DropdownMenu>
+						</UncontrolledDropdown>
+					</Nav>
+				</Collapse>
 			</Navbar>
-
-		</div>
-	)
+		</Fragment>
+	}
 }
-
-export default Navigation;
