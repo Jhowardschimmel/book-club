e60c38dd53a23c1fb7aa51216f222fb961129177
Jesse Schimmel <jhowardschimmel@gmail.com>
2020-07-19 16:54:18 -0400

    Add component did mount api call to google books api

--- client/src/components/pages/SearchPage/SearchPage.js
+++ client/src/components/pages/SearchPage/SearchPage.js
@@ -1,16 +1,37 @@
 import React from "react";
+import API from "../../../utils/API";
 import { Fragment } from 'react';
 import {
     Col,
     Container,
     Row
 } from "reactstrap";
+import Card from "../../Card";
 import Navigation from "../../Navigation";
 import Header from "../../Header";
 import Search from "../../Search";
 import Footer from "../../Footer";
 
 export default class SearchPage extends React.Component {
+    state = {
+        search: "",
+        results: [],
+        error: "",
+        title: "",
+        imageLink: "",
+        description: "",
+        authors: ""
+    };
+
+    componentDidMount() {
+        API.getTheHobbit().then(res => this.setState({
+            results: res,
+            title: res.data.items[0].volumeInfo.title,
+            imageLink: res.data.items[0].volumeInfo.imageLinks.thumbnail,
+            description: res.data.items[0].volumeInfo.description,
+            authors: res.data.items[0].volumeInfo.authors
+        }))
+    }
 
     render() {
         return (
@@ -24,6 +45,13 @@ export default class SearchPage extends React.Component {
                     <Row>
                         <Col sm="12 d-flex justify-content-center">
                             <Header />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="12">
+                            <Card title = {this.state.title}
+                            imageLink = {this.state.imageLink}
+                            authors = {this.state.authors} description = {this.state.description} />
                         </Col>
                     </Row>
                     <Row>
